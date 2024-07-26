800fa77750fc7d5c5a583e2cbfca1a0b2c0aa525
DAVID SIMON BOLIVAR CASTRO <david14847@gmail.com>
2024-07-25 19:35:45 -0500

    filename

--- src/components/base/FileInputBase.jsx
+++ src/components/base/FileInputBase.jsx
@@ -1,52 +1,69 @@
-import React, { useRef } from 'react';
-import { Button, Input } from '@nextui-org/react';
-import { getValueFromFieldFormik } from '../../lib/helpers/utils';
+import React, { useRef, useState } from "react";
+import { Button, Input } from "@nextui-org/react";
+import { getValueFromFieldFormik } from "../../lib/helpers/utils";
 
-export const FileInputBase = ({ field, form, label, onChangeFile, ...props }) => {
-    const fileInputRef = useRef(null);
+export const FileInputBase = ({
+  field,
+  form,
+  label,
+  onChangeFile,
+  ...props
+}) => {
+  const fileInputRef = useRef(null);
 
-    const handleClick = () => {
-        fileInputRef.current.click();
-    };
+  const [fileName, setFileName] = useState("");
 
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
+  const handleClick = () => {
+    fileInputRef.current.click();
+  };
 
-        if (!file) {
-            form.setFieldValue(field.name, '');
-            return;
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+
+    if (!file) {
+      form.setFieldValue(field.name, "");
+      setFileName("");
+      return;
+    }
+
+    form.setFieldValue(field.name, file);
+    setFileName(file.name);
+
+    if (onChangeFile) {
+      onChangeFile(file);
+    }
+  };
+
+  const hasError =
+    getValueFromFieldFormik(form.errors, field.name) &&
+    getValueFromFieldFormik(form.touched, field.name);
+
+  return (
+    <>
+      <Input
+        label={label}
+        size="md"
+        variant="bordered"
+        onClick={handleClick}
+        placeholder="Click para seleccionar archivo..."
+        contentRight={
+          <Button auto onClick={handleClick}>
+            Browse
+          </Button>
         }
-
-        form.setFieldValue(field.name, file);
-
-        if (onChangeFile) {
-            onChangeFile(file);
+        value={fileName}
+        {...props}
+        isInvalid={hasError}
+        errorMessage={
+          hasError && getValueFromFieldFormik(form.errors, field.name)
         }
-    };
-
-    const hasError = getValueFromFieldFormik(form.errors, field.name) && getValueFromFieldFormik(form.touched, field.name);
-
-    return (
-        <>
-            <Input
-                label={label}
-                size='md'
-                variant="bordered"
-                onClick={handleClick}
-                placeholder="Click para seleccionar archivo..."
-                contentRight={<Button auto onClick={handleClick}>Browse</Button>}
-                {...props}
-                isInvalid={hasError}
-                errorMessage={
-                    hasError && getValueFromFieldFormik(form.errors, field.name)
-                }
-            />
-            <input
-                type="file"
-                ref={fileInputRef}
-                style={{ display: 'none' }}
-                onChange={handleFileChange}
-            />
-        </>
-    );
+      />
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+    </>
+  );
 };
--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,15 +1,38 @@
+import { useEffect, useState } from "react";
 import axiosInstance from "../axios/axios";
 import useAsync from "./useAsync";
 
 export const useFetchData = ({ url, options = {}, dependencies = [] }) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  // const fetchData = async () => {
+  //   const resp = await axiosInstance.get(`${url}`);
+  //   return resp.data;
+  // };
+
+  // const { loading, error, data } = useAsync(async () => {
+  //   return await fetchData();
+  // }, dependencies);
+
   const fetchData = async () => {
-    const resp = await axiosInstance.get(`${url}`);
-    return resp.data;
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await axiosInstance.get(url, options);
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  const { loading, error, data } = useAsync(async () => {
-    return await fetchData();
-  }, dependencies);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return { loading, error, data, fetchData };
 };
